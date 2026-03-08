cae30fe403ad6990a0a7524f2ce74001e82e4dd8
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-08 22:02:39 +0000

    Added letter guessing validation logic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 #additional ideas to focus on later:
 #eventually, create word banks based on different lengths of words and ask user for input on which lenght they want
+#adjust number of lives: difficulty setting
 
 
 
@@ -24,27 +25,77 @@ def start_game():
         else:
             print("Unfortunately, that's an an invalid input.")
 
-game_started =  start_game()
+game_started = start_game()
 
 while game_started:
     print("The game is running")
+    
+    
+
+    #while game_is_active:
+    
+    
+    #select which word from word bank
+    
+    def select_word():
+        chosen_index = random.randint(0, len(word_bank) - 1)
+        return word_bank[chosen_index]
+    
+    chosen_word = select_word()
+    print("These are the rules of the game")
+    
+    
+    
+    
+    
+    
+    
     game_started = False
     
-
-#while game_is_active:
-
+    
+   
     
 
-#select which word from word bank
+#set chosen word to python, to test
+chosen_word = 'python'
 
-chosen_index = random.randint(0, len(word_bank) - 1)
-chosen_word = word_bank[chosen_index]
+lives = 6
+correct_guesses = 0
+guessed_letters = []
+display_word = ['_'] * len(chosen_word)
 
+while lives > 0: 
+    print(f"You currently have {lives} lives.")
+    print(f"You currently have {correct_guesses} correct_guesses.")
+    print("You have guessed the following letters:")
+    print(*guessed_letters)
+    print("Here's what you know so far:")
+    print(*display_word)
+    valid_guess = False
+    letter_guess = input("Please enter a letter guess: ")
+    if letter_guess.isalpha() and len(letter_guess) == 1:
+        valid_guess = True
+    else:
+        print()
+    
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    break
+print("Oh no! You have run out of lives!")
 
 
 
 #main game
 #display current state of word: blanks for unrevaled letters
+
+
 
 #limited number of incorrect guesses - typically X amount
 
