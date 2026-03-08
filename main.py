bcf8a5be11accea245005bf887f59c96d115c547
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-08 21:13:39 +0000

    Created start game logic

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 #Hangman Game Project
 import random
 
+#additional ideas to focus on later:
 #eventually, create word banks based on different lengths of words and ask user for input on which lenght they want
+
+
 
 #create a word bank (start with 6 letters)
 
 word_bank = ['python', 'guitar', 'coffee', 'breeze', 'window', 'galaxy', 'jungle', 'silver', 'orchid', 'poetry']
 
+#Initiate the first game
+
+total_session_games = 0
+
+while True:
+    start_game_response = input("Do you want to play hangman?\n YES: enter y\n NO: enter n")
+    if start_game_response == 'y':
+        game_is_active = True
+    elif start_game_response == 'n':
+        game_is_active = False
+    else:
+        print("Unfortunately, that's an an invalid input. Please enter y or n.")
+    
+    
+
+#while game_is_active:
+
+    
+
 #select which word from word bank
 
 chosen_index = random.randint(0, len(word_bank) - 1)
 chosen_word = word_bank[chosen_index]
-print(chosen_word)
+
+
+
 
 #main game
 #display current state of word: blanks for unrevaled letters
