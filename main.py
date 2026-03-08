60a8ee48e9d93a2402b1459ef9d7382922e0afca
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-08 23:09:46 +0000

    Added letter guessing result on lives

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import random
 #additional ideas to focus on later:
 #eventually, create word banks based on different lengths of words and ask user for input on which lenght they want
 #adjust number of lives: difficulty setting
+#adjust times/time depending on number ie 1 is time, times is 2+
 
+
+#Main lessons:
+    
+#Alternative to while True and return in functions, is while True and break.
 
 
 #create a word bank (start with 6 letters)
@@ -58,36 +63,53 @@ while game_started:
 
 #set chosen word to python, to test
 chosen_word = 'python'
-
 lives = 6
 correct_guesses = 0
 guessed_letters = []
 display_word = ['_'] * len(chosen_word)
 
+def letter_validation(total_guesses):
+    '''Inputs a letter guess from user and ensures both a.) it's a letter b.) it hasn't been guessed already.'''
+    while True:
+        letter_guess = input("\n\n\nPlease enter a letter guess: ")
+        if letter_guess.isalpha() and len(letter_guess) == 1 and letter_guess not in guessed_letters:
+            total_guesses += 1
+            return letter_guess, total_guesses
+        else:
+            print("\n\n\nThat guess was invalid.")
+
+def guess_result(letter_guess, lives, correct_guesses):
+    ''' here '''
+    guessed_letters.append(letter_guess)
+    if letter_guess in chosen_word:
+        print("\n\n\nCorrect guess!")
+        correct_guesses += 1
+        ocurrences = 0
+        for i, letter in enumerate(chosen_word):
+            if letter == letter_guess:
+                ocurrences += 1
+                display_word[i] = letter_guess
+        print(f"Your guess appears in the secret word {ocurrences} times!")
+    else:
+        print("\n\n\nIncorrect guess...Unlucky!")
+        lives -= 1
+    return lives, correct_guesses
+
+
+total_guesses = 0
 while lives > 0: 
-    print(f"You currently have {lives} lives.")
-    print(f"You currently have {correct_guesses} correct_guesses.")
-    print("You have guessed the following letters:")
-    print(*guessed_letters)
+    print(f"\n\n\nYou currently have {lives} lives.")
+    print(f"You currently have {correct_guesses} correct guesses.")
+    if total_guesses > 0:
+        print("You have guessed the following letters:")
+        print(*guessed_letters)
     print("Here's what you know so far:")
     print(*display_word)
-    valid_guess = False
-    letter_guess = input("Please enter a letter guess: ")
-    if letter_guess.isalpha() and len(letter_guess) == 1:
-        valid_guess = True
-    else:
-        print()
     
+    letter_guess, total_guesses = letter_validation(total_guesses)
+    lives, correct_guesses = guess_result(letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses)
     
         
-    
-    
-    
-    
-    
-    
-    
-    break
 print("Oh no! You have run out of lives!")
 
 
