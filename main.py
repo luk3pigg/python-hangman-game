c92e120b4e7cda650dde60bc4c6eeae27f9ea961
Luke Piggott <luk3.piggott6@gmail.com>
2026-03-09 00:09:40 +0000

    Added core loop functionality, game is working as it should

--- main.py
+++ main.py
@@ -16,11 +16,8 @@ import random
 
 word_bank = ['python', 'guitar', 'coffee', 'breeze', 'window', 'galaxy', 'jungle', 'silver', 'orchid', 'poetry']
 
-#Initiate the first game
-
-total_session_games = 0
-
 def start_game():
+    '''starts the game based on user input'''
     while True:
         start_game_response = input("Do you want to play hangman?\n YES: enter y\n NO: enter n\n").lower().strip()
         if start_game_response == 'y':
@@ -30,43 +27,10 @@ def start_game():
         else:
             print("Unfortunately, that's an an invalid input.")
 
-game_started = start_game()
-
-while game_started:
-    print("The game is running")
-    
-    
-
-    #while game_is_active:
-    
-    
-    #select which word from word bank
-    
-    def select_word():
-        chosen_index = random.randint(0, len(word_bank) - 1)
-        return word_bank[chosen_index]
-    
-    chosen_word = select_word()
-    print("These are the rules of the game")
-    
-    
-    
-    
-    
-    
-    
-    game_started = False
-    
-    
-   
-    
-
-#set chosen word to python, to test
-chosen_word = 'python'
-lives = 6
-correct_guesses = 0
-guessed_letters = []
-display_word = ['_'] * len(chosen_word)
+def select_word():
+    '''Selects a word at random from the word bank'''
+    chosen_index = random.randint(0, len(word_bank) - 1)
+    return word_bank[chosen_index]
 
 def letter_validation(total_guesses):
     '''Inputs a letter guess from user and ensures both a.) it's a letter b.) it hasn't been guessed already.'''
@@ -79,38 +43,55 @@ def letter_validation(total_guesses):
             print("\n\n\nThat guess was invalid.")
 
 def guess_result(letter_guess, lives, correct_guesses):
-    ''' here '''
+    '''Adds guess to guessed letters list, determines if guess is correct, and updates lives accordingly '''
     guessed_letters.append(letter_guess)
     if letter_guess in chosen_word:
         print("\n\n\nCorrect guess!")
-        correct_guesses += 1
         ocurrences = 0
         for i, letter in enumerate(chosen_word):
             if letter == letter_guess:
                 ocurrences += 1
                 display_word[i] = letter_guess
         print(f"Your guess appears in the secret word {ocurrences} times!")
+        correct_guesses += ocurrences
     else:
         print("\n\n\nIncorrect guess...Unlucky!")
         lives -= 1
     return lives, correct_guesses
 
-
-total_guesses = 0
-while lives > 0: 
-    print(f"\n\n\nYou currently have {lives} lives.")
-    print(f"You currently have {correct_guesses} correct guesses.")
-    if total_guesses > 0:
-        print("You have guessed the following letters:")
-        print(*guessed_letters)
-    print("Here's what you know so far:")
-    print(*display_word)
-    
-    letter_guess, total_guesses = letter_validation(total_guesses)
-    lives, correct_guesses = guess_result(letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses)
-    
+#starting the game
+total_session_games = 0
+game_active = start_game()
+while game_active:
+    chosen_word = select_word()
+    #would you like to know the rules of the game yes: y n: n
+    #Choose your settings: lives, word length, timer?
+    lives = 6
+    correct_guesses = 0
+    guessed_letters = []
+    display_word = ['_'] * len(chosen_word)
+    total_guesses = 0
+    game_won = False
+    #main game 
+    while lives > 0 and not game_won: 
+        print(f"\n\n\nYou currently have {lives} lives.")
+        print(f"You currently have {correct_guesses} correct guesses.")
+        if total_guesses > 0:
+            print("You have guessed the following letters:")
+            print(*guessed_letters)
+        print("Here's what you know so far:")
+        print(*display_word)
         
-print("Oh no! You have run out of lives!")
+        letter_guess, total_guesses = letter_validation(total_guesses)
+        lives, correct_guesses = guess_result(letter_guess=letter_guess, lives=lives, correct_guesses=correct_guesses)
+    
+        if correct_guesses == len(chosen_word):
+            game_won = True
+    
+    if game_won:
+        print("You won!")
+    else:
+        print("Oh no! You have run out of lives!")
 
 
 
